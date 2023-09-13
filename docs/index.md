ccaaec0252d071ff845d620ed7e77878033a6a65
Baptiste Pesquet <bpesquet@gmail.com>
2023-09-13 14:50:30 +0200

    Init project

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Notes on Artificial Intelligence
+
+Welcome to my online collection of notes related to AI. They were created for [teaching](https://www.bpesquet.fr/teaching/), [research](https://www.bpesquet.fr/research/), or personal interest. I have tried to write them in such a way that they should be accessible to anyone wanting to learn the subject, regardless of whether you are one of my students or not.
---
+++ src/foundations/proba_stats.py
@@ -0,0 +1,38 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# # Probability & Statistics
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Environment setup
+
+# %%
+import platform
+import torch
+
+print(f"Python version: {platform.python_version()}")
+print(f"PyTorch version: {torch.__version__}")
+
+# %% slideshow={"slide_type": "slide"}
+# Device configuration
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print(f"CUDA GPU {torch.cuda.get_device_name(0)} found :)")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+    print("Metal GPU found :)")
+else:
+    device = torch.device("cpu")
+    print("No available GPU :/")
