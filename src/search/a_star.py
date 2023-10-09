6a9451546abb1466c5052c6b75f5114a1efeeac0
Baptiste Pesquet <bpesquet@gmail.com>
2023-10-09 17:55:58 +0200

    Create A* chapter

---
+++ src/search/a_star.py
@@ -0,0 +1,36 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# # A* algorithm
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## Environment setup
+
+# %%
+import platform
+
+from IPython.display import YouTubeVideo
+
+print(f"Python version: {platform.python_version()}")
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ## A* on a real map
+#
+# City streets of Chicago and Rome. Intersections of streets represented as nodes and streets as edges.
+
+# %%
+YouTubeVideo("CgW0HPHqFE8")
+
+# %%
