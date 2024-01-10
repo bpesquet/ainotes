ebb6cda2821c7e7028d011a3f5938b012cc874df
Baptiste Pesquet <bpesquet@gmail.com>
2024-01-10 13:17:10 +0100

    Add classification example chapter

---
+++ src/ml/classification.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# # End-to-end classification example: recognize handwritten digits
+
+# %%
