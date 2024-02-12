0c4d7593aca2c30dd3352684bd20c90c2f8f3b6b
Baptiste Pesquet <bpesquet@gmail.com>
2024-02-12 15:01:42 +0100

    Finish first part of ANN chapter

--- src/ml/ann.py
+++ src/ml/ann.py
@@ -41,29 +41,23 @@ from torchvision.transforms import ToTensor
 sns.set_theme()
 
 
-# %% slideshow={"slide_type": "slide"}
-# Print environment info
-print(f"Python version: {platform.python_version()}")
-print(f"NumPy version: {np.__version__}")
-print(f"scikit-learn version: {sklearn.__version__}")
-print(f"PyTorch version: {torch.__version__}")
-
-
-# PyTorch device configuration
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-    print(f"CUDA GPU {torch.cuda.get_device_name(0)} found :)")
-# Performance issues exist with MPS backend
-# elif torch.backends.mps.is_available():
-#     device = torch.device("mps")
-#     print("Metal GPU found :)")
-else:
-    device = torch.device("cpu")
-    print("No GPU found, using CPU instead")
-
-
 # %% slideshow={"slide_type": "skip"}
 # Utility functions
+
+
+def plot_activation_function(f, f_prime, name, axis=(-6, 6, -1.1, 1.1)):
+    """Plot an activation function and its derivative"""
+
+    x_min, x_max = axis[0], axis[1]
+
+    z = np.linspace(x_min, x_max, 200)
+    plt.plot(z, f(z), "b-", linewidth=2, label=name)
+    plt.plot(z, f_prime(z), "g--", linewidth=2, label=f"{name}'")
+    plt.xlabel("x")
+    plt.ylabel(f"{name}(x)")
+    plt.axis(axis)
+    plt.legend(loc="upper left")
+    plt.show()
 
 
 def plot_dataset(x, y):
@@ -149,6 +143,27 @@ def plot_fashion_images(data, labels):
     plt.show()
 
 
+# %% slideshow={"slide_type": "slide"}
+# Print environment info
+print(f"Python version: {platform.python_version()}")
+print(f"NumPy version: {np.__version__}")
+print(f"scikit-learn version: {sklearn.__version__}")
+print(f"PyTorch version: {torch.__version__}")
+
+
+# PyTorch device configuration
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print(f"CUDA GPU {torch.cuda.get_device_name(0)} found :)")
+# Performance issues exist with MPS backend
+# elif torch.backends.mps.is_available():
+#     device = torch.device("mps")
+#     print("Metal GPU found :)")
+else:
+    device = torch.device("cpu")
+    print("No GPU found, using CPU instead")
+
+
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ## Fundamentals
 
@@ -158,10 +173,198 @@ def plot_fashion_images(data, labels):
 # [![TensorFlow playground](_images/tf_playground.png)](http://playground.tensorflow.org/#activation=tanh&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.1&regularizationRate=0&noise=0&networkShape=2&seed=0.59857&showTestData=false&discretize=false&percTrainData=30&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&showTestData_hide=false&problem_hide=true&regularization_hide=true&regularizationRate_hide=true&percTrainData_hide=false)
 
 # %% [markdown] slideshow={"slide_type": "slide"}
-# ### Activation functions
+# ### History
+#
+# #### A biological inspiration
+#
+# ![Neuron](_images/neuron.png)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### McCulloch & Pitts' formal neuron (1943)
+#
+# ![Formal neuron model](_images/neuron_model.jpeg)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Hebb's rule (1949)
+#
+# Attempt to explain synaptic plasticity, the adaptation of brain neurons during the learning process.
+#
+# > "The general idea is an old one, that any two cells or systems of cells that are repeatedly active at the same time will tend to become 'associated' so that activity in one facilitates activity in the other."
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Franck Rosenblatt's perceptron (1958)
+#
+# ![The Perceptron](_images/Perceptron.jpg)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### The perceptron learning algorithm
+#
+# 1. Init randomly the connection weights $\pmb{\omega}$.
+# 1. For each training sample $\pmb{x}^{(i)}$:
+#     1. Compute the perceptron output $y'^{(i)}$
+#     1. Adjust weights : $\pmb{\omega_{t+1}} = \pmb{\omega_t} + \eta (y^{(i)} - y'^{(i)}) \pmb{x}^{(i)}$
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Minsky's critic (1969)
+#
+# One perceptron cannot learn non-linearly separable functions.
+#
+# ![XOR problem](_images/xor.png)
+#
+# At the time, no learning algorithm existed for training the hidden layers of a MLP.
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Decisive breakthroughs (1970s-1990s)
+#
+# - 1974: backpropagation theory (P. Werbos).
+# - 1986: learning through backpropagation (Rumelhart, Hinton, Williams).
+# - 1991: universal approximation theorem (Hornik, Stinchcombe, White).
+# - 1989: first researchs on deep neural nets (LeCun, Bengio).
+# - 1991: Universal approximation theorem. Given appropriate complexity and appropriate learning, a network can theorically approximate any continuous function.
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ### Key components
+#
+# #### Anatomy of a fully connected network
+#
+# ![A neural network](_images/nn_weights.png)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Neuron output
+#
+# ![Neuron output](_images/neuron_output.png)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Activation functions
+#
+# They are applied to the weighted sum of neuron inputs to produce its output.
+#
+# They must be:
+#
+# - **non-linear**, so that the network has access to a richer representation space and not only linear transformations;
+# - **differentiable**, so that gradients can be computed during learning.
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ##### Sigmoid
+#
+# This function "squashes" its input between between 0 and 1, outputting something that can be interpreted as the probability of the positive class. It is often used in the final layer of the network for binary classification tasks.
+#
+# $$\sigma(x) = \frac{1}{1 + e^{-x}}$$
+#
+# $$\sigma'(x) = \frac{e^{-x}}{(1 + e^{-x})^2} = \sigma(x)\big(1 - \sigma(x)\big)$$
+
+
+# %% slideshow={"slide_type": "slide"}
+def sigmoid(x):
+    """Sigmoid function"""
+
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_prime(x):
+    """Derivative of the sigmoid function"""
+
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+# %% slideshow={"slide_type": "-"}
+plot_activation_function(sigmoid, sigmoid_prime, "Sigmoid", axis=(-6, 6, -0.1, 1.1))
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ##### tanh
+#
+# The hyperbolic tangent function has a similar shape as sigmoid, but outputs values in the $[-1,1]$ interval.
+#
+# $$tanh(x) = \frac{sinh(x)}{cosh(x)} = \frac{e^x - e^{-x}}{e^x + e^{-x}} = \frac{2}{1+e^{-2x}} -1 = 2\sigma(2x) - 1$$
+#
+# $$tanh'(x) = \frac{4}{(e^x + e^{-x})^2} = \frac{1}{cosh^2(x)}$$
+
+
+# %% slideshow={"slide_type": "slide"}
+def tanh(x):
+    """Hyperbolic tangent function"""
+
+    return 2 * sigmoid(2 * x) - 1
+
+
+def tanh_prime(x):
+    """Derivative of hyperbolic tangent function"""
+
+    return 4 / np.square(np.exp(x) + np.exp(-x))
+
+
+# %% slideshow={"slide_type": "-"}
+plot_activation_function(tanh, tanh_prime, "Tanh", axis=(-6, 6, -1.1, 1.1))
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# ##### ReLU
+#
+# The Rectified Linear Unit function has replaced sigmoid and tanh as the default activation function in most contexts.
+#
+# $$ReLU(x) = max(0,x)$$
+#
+# $$ReLU'(x) =
+#     \begin{cases}
+#       0 \; \forall x \in\; ]-\infty, 0] \\
+#       1 \; \forall x \in\; ]0, +\infty[
+#     \end{cases}$$
+
+
+# %% slideshow={"slide_type": "slide"}
+def relu(x):
+    """Rectified Linear Unit function"""
+
+    return np.maximum(0, x)
+
+
+def relu_prime(x):
+    """Derivative of the Rectified Linear Unit function"""
+
+    # https://stackoverflow.com/a/45022037
+    return (x > 0).astype(x.dtype)
+
+
+# %% slideshow={"slide_type": "-"}
+plot_activation_function(relu, relu_prime, "ReLU", axis=(-6, 6, -1.1, 6.1))
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ### Training process
+#
+# #### Learning algorithm
+#
+# [![Extract from the book Deep Learning with Python](_images/nn_learning.jpg)](https://www.manning.com/books/deep-learning-with-python)
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Weights initialization
+#
+# To facilitate training, initial weights must be:
+#
+# - non-zero
+# - random
+# - have small values
+#
+# [Several techniques exist](https://towardsdatascience.com/weight-initialization-in-neural-networks-a-journey-from-the-basics-to-kaiming-954fb9b47c79). A commonly used one is [Xavier initialization](https://proceedings.mlr.press/v9/glorot10a.html).
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Weights update
+#
+# Objective: minimize the loss function. Method: [gradient descent](principles.ipynb).
+#
+# $$\pmb{\omega_{t+1}} = \pmb{\omega_t} - \eta\nabla_{\pmb{\omega}}\mathcal{L}(\pmb{\omega_t})$$
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Backpropagation
+#
+# Objective: compute $\nabla_{\pmb{\omega}}\mathcal{L}(\pmb{\omega_t})$, the loss function gradient w.r.t. all the network weights.
+#
+# Method: apply the [chain rule](https://en.wikipedia.org/wiki/Chain_rule) to compute partial derivatives backwards, starting from the current output.
+#
+# $$y = f(g(x)) \;\;\;\; \frac{\partial y}{\partial x} = \frac{\partial f}{\partial g} \frac{\partial g}{\partial x}\;\;\;\; \frac{\partial y}{\partial x} = \sum_{i=1}^n \frac{\partial f}{\partial g^{(i)}} \frac{\partial g^{(i)}}{\partial x}$$
+
+# %% [markdown] slideshow={"slide_type": "slide"}
+# #### Visual demo of backpropagation
+#
+# [![Backprop explained visually](_images/visual_backprop_demo.png)](https://developers-dot-devsite-v2-prod.appspot.com/machine-learning/crash-course/backprop-scroll)
 
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ## BInary classification
@@ -169,7 +372,7 @@ def plot_fashion_images(data, labels):
 # %% [markdown] slideshow={"slide_type": "slide"}
 # ### Data generation and visualization
 #
-# A [scikit-learn function](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html) is used to easily generate 2-dimensional data with two classes.
+# A scikit-learn [function](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_circles.html) is used to easily generate 2-dimensional data with two classes.
 
 # %% slideshow={"slide_type": "-"}
 # Generate 2D data (a large circle containing a smaller circle)
